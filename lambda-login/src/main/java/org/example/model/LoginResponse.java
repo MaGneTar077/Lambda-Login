818a15daa2c9ab79a344bed83db7bbb75877dff0
MaGneTar077 <lgsantiago07@gmail.com>
2025-05-24 18:57:45 -0500

    Creacion de la capa de modelo con las clases LoginRequest y LoginResponse

---
+++ lambda-login/src/main/java/org/example/model/LoginResponse.java
@@ -0,0 +1,28 @@
+package org.example.model;
+
+public class LoginResponse {
+
+    public String message;
+    public String token;
+
+    public LoginResponse(String message, String token) {
+        this.message = message;
+        this.token = token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
